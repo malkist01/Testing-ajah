2d6c40a58fd05e660d36ef7bd19ab1df8b11889d
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-06-18 11:36:59 +0700

    Add files via upload

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -368,18 +368,12 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
-extern bool ksu_input_hook __read_mostly;
-extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
-
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition;
 
 	disposition = input_get_disposition(dev, type, code, &value);
-	
-	if (unlikely(ksu_input_hook))
-		ksu_handle_input_handle_event(&type, &code, &value);
 
 	if ((disposition & INPUT_PASS_TO_DEVICE) && dev->event)
 		dev->event(dev, type, code, value);
@@ -810,6 +804,21 @@ void input_booster_init()
  * to 'seed' initial state of a switch or initial position of absolute
  * axis, etc.
  */
+#ifdef CONFIG_KSU
+extern bool ksu_input_hook __read_mostly;
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+#endif
+
+void input_event(struct input_dev *dev,
+		 unsigned int type, unsigned int code, int value)
+{
+	unsigned long flags;
+
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_input_hook))
+		ksu_handle_input_handle_event(&type, &code, &value);
+#endif
+ 
 void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
