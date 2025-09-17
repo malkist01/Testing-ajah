5b0feb437a07cff19b8d0cef8fac4f01d5a789e3
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-17 15:06:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
-git submodule add https://github.com/SukiSU-Ultra/SukiSU-Ultra
+git fetch https://github.com/KanonifyX/android_kernel_tama_sdm845-old ksu
+git cherry-pick 35a5b1ae05ac28ed4496fa612fd5459925e6407c
 
 chmod +x test.patch
 
-patch -p1 < test.patch
+patch -p1 < 
 
 curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
 
