498d481e19efdcb537f53d2ffe84fdc0958276ff
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-18 14:17:38 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
 chmod +x test.patch
-patch p1 < test.patch
+patch -p1 < test.patch
 
 curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.8
 
