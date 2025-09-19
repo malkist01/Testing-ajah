33a38899b7bec0dc3f2471475803fd19ad97b378
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-19 11:45:01 +0700

    Create applyPatches.sh

---
+++ ksu/patches/applyPatches.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# hdjsjfjjwufbeizihfjejzf
+
+export maindir="$(pwd)"
+export outside="${maindir}/.."
+source "${outside}/$1env"
+
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/refs/heads/main/kernel/setup.sh" | bash -
+git add . && git commit -am "drivers: KernelSU"
+KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
+KSU_ver=$(($KSU_git_ver + 10000 + 200))
+
+patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+if [[ -d "$patchesdir" ]]; then
+  for patch_file in "$patchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
+
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "${defconfig_file}"
+
+echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
+
+echo -e " \nincludes rsuntk's KernelSU fork, ver ${KSU_ver}" >> banner_append
+
