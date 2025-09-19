6a9a76fa26719fff22bfe120618cd7f13c06198f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-19 11:36:42 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,16 @@ git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
+patchesdir="ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+if [[ -d "$patchesdir" ]]; then
+  for patch_file in "$patchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
+
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
