53f85c499a4d23f1957d0dbab8df9c8d15044383
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-20 15:24:06 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ rm -rf kernel
 git clone $REPO -b $BRANCH kernel 
 cd kernel
 
-curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSUonArm32/main/kernel/setup.sh" | bash -s main
-
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
 echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
