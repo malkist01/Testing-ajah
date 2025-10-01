5ff10278204e7eee722400665719423bace5b3a3
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-02 03:01:27 +0700

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -561,7 +561,6 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 
 #ifdef CONFIG_KSU
 extern bool ksu_vfs_read_hook __read_mostly;
-__attribute__((cold))
 extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
 			size_t *count_ptr);
 #endif
@@ -576,7 +575,7 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 #ifdef CONFIG_KSU
 		if (unlikely(ksu_vfs_read_hook)) 
 			ksu_handle_sys_read(fd, &buf, &count);
-#endif	
+#endif
 		ret = vfs_read(f.file, buf, count, &pos);
 		if (ret >= 0)
 			file_pos_write(f.file, pos);
