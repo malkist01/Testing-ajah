8fedbeb5e899f28e2dcffd395f2300bf2b44ffb8
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-04 20:54:44 +0700

    Update security.c

--- security/security.c
+++ security/security.c
@@ -138,15 +138,10 @@ int __init register_security(struct security_operations *ops)
 
 #ifdef CONFIG_KSU
 extern int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
-		     unsigned long arg4, unsigned long arg5);
+ 		     unsigned long arg4, unsigned long arg5);
 extern int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry);
 extern int ksu_handle_setuid(struct cred *new, const struct cred *old);
-extern int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
-			      unsigned perm);
-extern int ksu_sb_mount(const char *dev_name, const struct path *path,
-                        const char *type, unsigned long flags, void *data);
 #endif
-
 /* Security operations */
 
 int security_binder_set_context_mgr(struct task_struct *mgr)
