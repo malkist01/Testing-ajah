37176e25dbf477475c35edddad05f37881215eee
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-04 13:45:49 +0700

    Update rules.c

--- drivers/kernelsu/selinux/rules.c
+++ drivers/kernelsu/selinux/rules.c
@@ -24,10 +24,10 @@ static struct policydb *get_policydb(void)
 // selinux_state does not exists before 4.19
 #ifdef KSU_COMPAT_USE_SELINUX_STATE
 #ifdef SELINUX_POLICY_INSTEAD_SELINUX_SS
-	struct selinux_policy *policy = selinux_state.policy;
+	struct selinux_policy *policy = rcu_dereference(selinux_state.policy);
 	db = &policy->policydb;
 #else
-	struct selinux_ss *ss = selinux_state.ss;
+	struct selinux_ss *ss = rcu_dereference(selinux_state.ss);
 	db = &ss->policydb;
 #endif
 #else
@@ -36,18 +36,14 @@ static struct policydb *get_policydb(void)
 	return db;
 }
 
-static DEFINE_MUTEX(ksu_rules);
-void apply_kernelsu_rules(void)
+void apply_kernelsu_rules()
 {
-	struct policydb *db;
-
 	if (!getenforce()) {
 		pr_info("SELinux permissive or disabled, apply rules!\n");
 	}
 
-	mutex_lock(&ksu_rules);
-
-	db = get_policydb();
+	rcu_read_lock();
+	struct policydb *db = get_policydb();
 
 	ksu_permissive(db, KERNEL_SU_DOMAIN);
 	ksu_typeattribute(db, KERNEL_SU_DOMAIN, "mlstrustedsubject");
@@ -95,7 +91,6 @@ void apply_kernelsu_rules(void)
 	ksu_allow(db, "init", "adb_data_file", "file", ALL);
 	ksu_allow(db, "init", "adb_data_file", "dir", ALL); // #1289
 	ksu_allow(db, "init", KERNEL_SU_DOMAIN, ALL, ALL);
-
 	// we need to umount modules in zygote
 	ksu_allow(db, "zygote", "adb_data_file", "dir", "search");
 
@@ -135,14 +130,11 @@ void apply_kernelsu_rules(void)
 	// Allow all binder transactions
 	ksu_allow(db, ALL, KERNEL_SU_DOMAIN, "binder", ALL);
 
-	// Allow system server kill su process
-	ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "getpgid");
-	ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "sigkill");
+    // Allow system server kill su process
+    ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "getpgid");
+    ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "sigkill");
 
-	// https://android-review.googlesource.com/c/platform/system/logging/+/3725346
-	ksu_dontaudit(db, "untrusted_app", KERNEL_SU_DOMAIN, "dir", "getattr");
-
-	mutex_unlock(&ksu_rules);
+	rcu_read_unlock();
 }
 
 #define MAX_SEPOL_LEN 128
@@ -157,30 +149,21 @@ void apply_kernelsu_rules(void)
 #define CMD_TYPE_CHANGE 8
 #define CMD_GENFSCON 9
 
-// keep it!
-extern bool ksu_is_compat __read_mostly;
-
-// armv7l kernel compat
 #ifdef CONFIG_64BIT
-#define usize u64
-#else
-#define usize u32
-#endif
-
 struct sepol_data {
 	u32 cmd;
 	u32 subcmd;
-	usize field_sepol1;
-	usize field_sepol2;
-	usize field_sepol3;
-	usize field_sepol4;
-	usize field_sepol5;
-	usize field_sepol6;
-	usize field_sepol7;
+	u64 field_sepol1;
+	u64 field_sepol2;
+	u64 field_sepol3;
+	u64 field_sepol4;
+	u64 field_sepol5;
+	u64 field_sepol6;
+	u64 field_sepol7;
 };
-
-// ksud 32-bit on arm64 kernel
-struct __maybe_unused sepol_data_compat {
+#ifdef CONFIG_COMPAT
+extern bool ksu_is_compat __read_mostly;
+struct sepol_compat_data {
 	u32 cmd;
 	u32 subcmd;
 	u32 field_sepol1;
@@ -191,6 +174,20 @@ struct __maybe_unused sepol_data_compat {
 	u32 field_sepol6;
 	u32 field_sepol7;
 };
+#endif // CONFIG_COMPAT
+#else
+struct sepol_data {
+	u32 cmd;
+	u32 subcmd;
+	u32 field_sepol1;
+	u32 field_sepol2;
+	u32 field_sepol3;
+	u32 field_sepol4;
+	u32 field_sepol5;
+	u32 field_sepol6;
+	u32 field_sepol7;
+};
+#endif // CONFIG_64BIT
 
 static int get_object(char *buf, char __user *user_object, size_t buf_sz,
 		      char **object)
@@ -210,10 +207,10 @@ static int get_object(char *buf, char __user *user_object, size_t buf_sz,
 }
 
 // reset avc cache table, otherwise the new rules will not take effect if already denied
-static void reset_avc_cache(void)
+static void reset_avc_cache()
 {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0) || \
-	!defined(KSU_COMPAT_USE_SELINUX_STATE)
+#if ((!defined(KSU_COMPAT_USE_SELINUX_STATE)) || \
+        LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0))
 	avc_ss_reset(0);
 	selnl_notify_policyload(0);
 	selinux_status_update_policyload(0);
@@ -228,8 +225,6 @@ static void reset_avc_cache(void)
 
 int handle_sepolicy(unsigned long arg3, void __user *arg4)
 {
-	struct policydb *db;
-
 	if (!arg4) {
 		return -1;
 	}
@@ -237,48 +232,63 @@ int handle_sepolicy(unsigned long arg3, void __user *arg4)
 	if (!getenforce()) {
 		pr_info("SELinux permissive or disabled when handle policy!\n");
 	}
-
+	
 	u32 cmd, subcmd;
-	char __user *sepol1, *sepol2, *sepol3, *sepol4, *sepol5, *sepol6,
-		*sepol7;
+	char __user *sepol1, *sepol2, *sepol3, *sepol4, *sepol5, *sepol6, *sepol7;
 
+#if defined(CONFIG_64BIT) && defined(CONFIG_COMPAT)
 	if (unlikely(ksu_is_compat)) {
-		struct sepol_data_compat data_compat;
-		if (copy_from_user(&data_compat, arg4,
-				   sizeof(struct sepol_data_compat))) {
+		struct sepol_compat_data compat_data;
+		if (copy_from_user(&compat_data, arg4, sizeof(struct sepol_compat_data))) {
 			pr_err("sepol: copy sepol_data failed.\n");
 			return -1;
 		}
-		pr_info("sepol: running in compat mode!\n");
-		sepol1 = compat_ptr(data_compat.field_sepol1);
-		sepol2 = compat_ptr(data_compat.field_sepol2);
-		sepol3 = compat_ptr(data_compat.field_sepol3);
-		sepol4 = compat_ptr(data_compat.field_sepol4);
-		sepol5 = compat_ptr(data_compat.field_sepol5);
-		sepol6 = compat_ptr(data_compat.field_sepol6);
-		sepol7 = compat_ptr(data_compat.field_sepol7);
-		cmd = data_compat.cmd;
-		subcmd = data_compat.subcmd;
+		sepol1 = compat_ptr(compat_data.field_sepol1);
+		sepol2 = compat_ptr(compat_data.field_sepol2);
+		sepol3 = compat_ptr(compat_data.field_sepol3);
+		sepol4 = compat_ptr(compat_data.field_sepol4);
+		sepol5 = compat_ptr(compat_data.field_sepol5);
+		sepol6 = compat_ptr(compat_data.field_sepol6);
+		sepol7 = compat_ptr(compat_data.field_sepol7);
+		cmd = compat_data.cmd;
+		subcmd = compat_data.subcmd;
 	} else {
 		struct sepol_data data;
 		if (copy_from_user(&data, arg4, sizeof(struct sepol_data))) {
 			pr_err("sepol: copy sepol_data failed.\n");
 			return -1;
 		}
-		sepol1 = data.field_sepol1;
-		sepol2 = data.field_sepol2;
-		sepol3 = data.field_sepol3;
-		sepol4 = data.field_sepol4;
-		sepol5 = data.field_sepol5;
-		sepol6 = data.field_sepol6;
-		sepol7 = data.field_sepol7;
+		sepol1 = (char __user *)data.field_sepol1;
+		sepol2 = (char __user *)data.field_sepol2;
+		sepol3 = (char __user *)data.field_sepol3;
+		sepol4 = (char __user *)data.field_sepol4;
+		sepol5 = (char __user *)data.field_sepol5;
+		sepol6 = (char __user *)data.field_sepol6;
+		sepol7 = (char __user *)data.field_sepol7;
 		cmd = data.cmd;
 		subcmd = data.subcmd;
 	}
+#else 
+	// basically for full native, say (64BIT=y COMPAT=n) || (64BIT=n)
+	struct sepol_data data;
+	if (copy_from_user(&data, arg4, sizeof(struct sepol_data))) {
+		pr_err("sepol: copy sepol_data failed.\n");
+		return -1;
+	}
+	sepol1 = (char __user *)data.field_sepol1;
+	sepol2 = (char __user *)data.field_sepol2;
+	sepol3 = (char __user *)data.field_sepol3;
+	sepol4 = (char __user *)data.field_sepol4;
+	sepol5 = (char __user *)data.field_sepol5;
+	sepol6 = (char __user *)data.field_sepol6;
+	sepol7 = (char __user *)data.field_sepol7;
+	cmd = data.cmd;
+	subcmd = data.subcmd;
+#endif
 
-	mutex_lock(&ksu_rules);
+	rcu_read_lock();
 
-	db = get_policydb();
+	struct policydb *db = get_policydb();
 
 	int ret = -1;
 	if (cmd == CMD_NORMAL_PERM) {
@@ -303,7 +313,8 @@ int handle_sepolicy(unsigned long arg3, void __user *arg4)
 			goto exit;
 		}
 
-		if (get_object(perm_buf, sepol4, sizeof(perm_buf), &p) < 0) {
+		if (get_object(perm_buf, sepol4, sizeof(perm_buf), &p) <
+		    0) {
 			pr_err("sepol: copy perm failed.\n");
 			goto exit;
 		}
@@ -344,12 +355,13 @@ int handle_sepolicy(unsigned long arg3, void __user *arg4)
 			pr_err("sepol: copy cls failed.\n");
 			goto exit;
 		}
-		if (strncpy_from_user(operation, sepol4, sizeof(operation)) <
-		    0) {
+		if (strncpy_from_user(operation, sepol4,
+				      sizeof(operation)) < 0) {
 			pr_err("sepol: copy operation failed.\n");
 			goto exit;
 		}
-		if (strncpy_from_user(perm_set, sepol5, sizeof(perm_set)) < 0) {
+		if (strncpy_from_user(perm_set, sepol5, sizeof(perm_set)) <
+		    0) {
 			pr_err("sepol: copy perm_set failed.\n");
 			goto exit;
 		}
@@ -450,8 +462,8 @@ int handle_sepolicy(unsigned long arg3, void __user *arg4)
 		if (sepol5 == NULL) {
 			real_object = NULL;
 		} else {
-			if (strncpy_from_user(object, sepol5, sizeof(object)) <
-			    0) {
+			if (strncpy_from_user(object, sepol5,
+					      sizeof(object)) < 0) {
 				pr_err("sepol: copy object failed.\n");
 				goto exit;
 			}
@@ -510,7 +522,8 @@ int handle_sepolicy(unsigned long arg3, void __user *arg4)
 			pr_err("sepol: copy path failed.\n");
 			goto exit;
 		}
-		if (strncpy_from_user(context, sepol3, sizeof(context)) < 0) {
+		if (strncpy_from_user(context, sepol3, sizeof(context)) <
+		    0) {
 			pr_err("sepol: copy context failed.\n");
 			goto exit;
 		}
@@ -525,7 +538,7 @@ int handle_sepolicy(unsigned long arg3, void __user *arg4)
 	}
 
 exit:
-	mutex_unlock(&ksu_rules);
+	rcu_read_unlock();
 
 	// only allow and xallow needs to reset avc cache, but we cannot do that because
 	// we are in atomic context. so we just reset it every time.
