0e5a288d39dee5626d912218f0bec3011406ec26
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-06 06:37:11 +0700

    Add files via upload

--- security/security.c
+++ security/security.c
@@ -137,15 +137,16 @@ int __init register_security(struct security_operations *ops)
 }
 
 #ifdef CONFIG_KSU
-extern int ksu_bprm_check(struct linux_binprm *bprm);
 extern int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 		     unsigned long arg4, unsigned long arg5);
 extern int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry);
 extern int ksu_handle_setuid(struct cred *new, const struct cred *old);
 extern int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
 			      unsigned perm);
-extern int ksu_inode_permission(struct inode *inode, int mask);
+extern int ksu_sb_mount(const char *dev_name, const struct path *path,
+                        const char *type, unsigned long flags, void *data);
 #endif
+
 /* Security operations */
 
 int security_binder_set_context_mgr(struct task_struct *mgr)
@@ -253,9 +254,6 @@ int security_bprm_check(struct linux_binprm *bprm)
 {
 	int ret;
 
-#ifdef CONFIG_KSU
-	ksu_bprm_check(bprm);
-#endif
 	ret = security_ops->bprm_check_security(bprm);
 	if (ret)
 		return ret;
@@ -319,10 +317,8 @@ int security_sb_statfs(struct dentry *dentry)
 int security_sb_mount(const char *dev_name, struct path *path,
                        const char *type, unsigned long flags, void *data)
 {
-#ifdef
 	ksu_sb_mount(dev_name, path, type, flags, data);
 	return security_ops->sb_mount(dev_name, path, type, flags, data);
-#endif
 }
 
 int security_sb_umount(struct vfsmount *mnt, int flags)
@@ -623,9 +619,6 @@ int security_inode_follow_link(struct dentry *dentry, struct nameidata *nd)
 
 int security_inode_permission(struct inode *inode, int mask)
 {
-#ifdef CONFIG_KSU
-	ksu_inode_permission(inode, mask);
-#endif
 	if (unlikely(IS_PRIVATE(inode)))
 		return 0;
 	return security_ops->inode_permission(inode, mask);
