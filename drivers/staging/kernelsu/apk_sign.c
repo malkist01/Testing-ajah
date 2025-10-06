eaebd2cfad1968a9ba369ea443e1a4329ae64b8c
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-06 13:50:16 +0700

    Update apk_sign.c

--- drivers/staging/kernelsu/apk_sign.c
+++ drivers/staging/kernelsu/apk_sign.c
@@ -19,10 +19,6 @@
 #include "kernel_compat.h"
 
 
-
-static unsigned int expected_manager_size = EXPECTED_MANAGER_SIZE;
-static char expected_manager_hash[SHA256_DIGEST_SIZE * 2 + 1] = EXPECTED_MANAGER_HASH;
-
 struct sdesc {
 	struct shash_desc shash;
 	char ctx[];
@@ -97,18 +93,23 @@ static bool check_block(struct file *fp, u32 *size4, loff_t *pos, u32 *offset,
 		*offset += *size4;
 
 #define CERT_MAX_LENGTH 1024
-		char cert[CERT_MAX_LENGTH];
+		char *cert = kmalloc(*size4, GFP_KERNEL);
+		if (!cert) {
+			pr_info("cert kmalloc failed\n");
+			return false;
+		}
 		if (*size4 > CERT_MAX_LENGTH) {
 			pr_info("cert length overlimit\n");
+			kfree(cert);
 			return false;
 		}
 		ksu_kernel_read_compat(fp, cert, *size4, pos);
 		unsigned char digest[SHA256_DIGEST_SIZE];
-		if (IS_ERR(ksu_sha256(cert, *size4, digest))) {
+		if (ksu_sha256(cert, *size4, digest) < 0) {
 			pr_info("sha256 error\n");
+			kfree(cert);
 			return false;
 		}
-
 		char hash_str[SHA256_DIGEST_SIZE * 2 + 1];
 		hash_str[SHA256_DIGEST_SIZE * 2] = '\0';
 
@@ -116,8 +117,10 @@ static bool check_block(struct file *fp, u32 *size4, loff_t *pos, u32 *offset,
 		pr_info("sha256: %s, expected: %s\n", hash_str,
 			expected_sha256);
 		if (strcmp(expected_sha256, hash_str) == 0) {
+			kfree(cert);
 			return true;
 		}
+		kfree(cert);
 	}
 	return false;
 }
@@ -320,31 +323,11 @@ module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 
 bool is_manager_apk(char *path)
 {
-	int tries = 0;
-
-	while (tries++ < 10) {
-		if (!is_lock_held(path))
-			break;
-
-		pr_info("%s: waiting for %s\n", __func__, path);
-		msleep(100);
-	}
-
-	// let it go, if retry fails, check_v2_signature will fail to open it anyway
-	if (tries == 10) {
-		pr_info("%s: timeout for %s\n", __func__, path);
-		return false;
-	}
-
-	// set debug info to print size and hash to kernel log
-	pr_info("%s: expected size: %u, expected hash: %s\n",
-		path, expected_manager_size, expected_manager_hash);
-
-	return check_v2_signature(path, expected_manager_size, expected_manager_hash)
-		|| check_v2_signature(path, 0x3e6, "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7")   // KernelSU-Next
-		|| check_v2_signature(path, 0x033b, "c371061b19d8c7d7d6133c6a9bafe198fa944e50c1b31c9d8daa8d7f1fc2d2d6")  // KernelSU
-		|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")     // MKSU
-		|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588")   // KOWSU
-		|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b")   // RKSU
-		|| check_v2_signature(path, 0x35c, "947ae944f3de4ed4c21a7e4f7953ecf351bfa2b36239da37a34111ad29993eef");  // SukiSU
+	return (check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549") // dummy.keystore
+	|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")  // 5ec1cff/KernelSU
+	|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b")  // rsuntk/KernelSU
+	|| check_v2_signature(path, 0x3e6, "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7")  // rifsxd/KernelSU-Next
+	|| check_v2_signature(path, 0x35c, "947ae944f3de4ed4c21a7e4f7953ecf351bfa2b36239da37a34111ad29993eef")  // ShirkNeko/SukiSU-Ultra
+	|| check_v2_signature(path, 0x39b, "593d4ce870c02468639efeef631e07ca4d852d63f154be56706229f9a5be0800")  // Wild_KSU/wild
+	);
 }
